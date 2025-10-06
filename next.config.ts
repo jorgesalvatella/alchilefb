14ec5c9aa2784348a65780c04f0d46d1499a82c2
Jorge Humberto Espinosa Salvatella <humberto@nobbora.com>
2025-10-06 05:30:47 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'imagenes.nobbora.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
