3b9436df0a4c5971a9421ac8f615ba897f129c70
Jorge Humberto Espinosa Salvatella <humberto@nobbora.com>
2025-10-06 04:46:16 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9824031244-700aa",
+  "appId": "1:1073493631859:web:d747356460c06013eb5b06",
+  "apiKey": "AIzaSyCetC4ZTnHKQa2Pm_YWfhoMCbYqdaGTqQc",
+  "authDomain": "studio-9824031244-700aa.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1073493631859"
+};
